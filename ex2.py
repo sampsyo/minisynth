ae8d0666a1f2bc31f808d46a70539618174da24c
Adrian Sampson <adrian@radbox.org>
2018-05-08 15:02:04 -0400

    Variable lookups

--- ex2.py
+++ ex2.py
@@ -17,21 +17,25 @@ GRAMMAR = """
 
 ?item: NUMBER      -> num
   | "-" item       -> neg
+  | WORD           -> var
   | "(" sum ")"
 
 %import common.NUMBER
 %import common.WS
+%import common.WORD
 %ignore WS
 """.strip()
 
 
-def interp(tree):
+def interp(tree, lookup):
     """Evaluate the arithmetic expression.
+
     """
 
     op = tree.data
     if op in ('add', 'sub', 'mul', 'div', 'shl', 'shr'):  # Binary operators.
-        lhs, rhs = interp(tree.children[0]), interp(tree.children[1])
+        lhs = interp(tree.children[0], lookup)
+        rhs = interp(tree.children[1], lookup)
         if op == 'add':
             return lhs + rhs
         elif op == 'sub':
@@ -45,16 +49,18 @@ def interp(tree):
         elif op == 'shr':
             return lhs >> rhs
     elif op == 'neg':  # Negation.
-        sub = interp(tree.children[0])
+        sub = interp(tree.children[0], lookup)
         return -sub
     elif op == 'num':  # Literal number.
         return int(tree.children[0])
+    elif op == 'var':  # Variable lookup.
+        return lookup(tree.children[0])
 
 
 def ex2():
     parser = lark.Lark(GRAMMAR)
-    tree = parser.parse("1 * 2 + (1 - 5) << 1")
-    print(interp(tree))
+    tree = parser.parse("1 * 2 + (1 - x) << 1")
+    print(interp(tree, lambda _: 9))
 
 
 if __name__ == '__main__':
