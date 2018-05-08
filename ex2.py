b4e3addd321e6858682670e91fb0f3c2ecf222e3
Adrian Sampson <adrian@radbox.org>
2018-05-08 14:51:11 -0400

    Add shifts

--- ex2.py
+++ ex2.py
@@ -5,16 +5,18 @@ import lark
 GRAMMAR = """
 ?start: sum
 
-?sum: product
-  | sum "+" product  -> add
-  | sum "-" product  -> sub
+?sum: term
+  | sum "+" term   -> add
+  | sum "-" term   -> sub
 
-?product: item
-  | product "*" item -> mul
-  | product "/" item -> div
+?term: item
+  | term "*"  item -> mul
+  | term "/"  item -> div
+  | term ">>" item -> shr
+  | term "<<" item -> shl
 
-?item: NUMBER        -> num
-  | "-" item         -> neg
+?item: NUMBER      -> num
+  | "-" item       -> neg
   | "(" sum ")"
 
 %import common.NUMBER
@@ -24,28 +26,34 @@ GRAMMAR = """
 
 
 def interp(tree):
-    if tree.data == 'add':
+    """Evaluate the arithmetic expression.
+    """
+
+    op = tree.data
+    if op in ('add', 'sub', 'mul', 'div', 'shl', 'shr'):  # Binary operators.
         lhs, rhs = interp(tree.children[0]), interp(tree.children[1])
-        return lhs + rhs
-    elif tree.data == 'sub':
-        lhs, rhs = interp(tree.children[0]), interp(tree.children[1])
-        return lhs - rhs
-    elif tree.data == 'mul':
-        lhs, rhs = interp(tree.children[0]), interp(tree.children[1])
-        return lhs * rhs
-    elif tree.data == 'div':
-        lhs, rhs = interp(tree.children[0]), interp(tree.children[1])
-        return lhs / rhs
-    elif tree.data == 'neg':
+        if op == 'add':
+            return lhs + rhs
+        elif op == 'sub':
+            return lhs - rhs
+        elif op == 'mul':
+            return lhs * rhs
+        elif op == 'div':
+            return lhs / rhs
+        elif op == 'shl':
+            return lhs << rhs
+        elif op == 'shr':
+            return lhs >> rhs
+    elif op == 'neg':  # Negation.
         sub = interp(tree.children[0])
         return -sub
-    elif tree.data == 'num':
+    elif op == 'num':  # Literal number.
         return int(tree.children[0])
 
 
 def ex2():
     parser = lark.Lark(GRAMMAR)
-    tree = parser.parse("1 * 2 + (1 - 5)")
+    tree = parser.parse("1 * 2 + (1 - 5) << 1")
     print(interp(tree))
 
 
